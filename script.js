ce9b88cc7b7ba387558793ef36baa31cb3695b6c
benedivI-lab <benedivilunga@gmail.com>
2026-02-07 20:46:57 -0600

    chnages for artist page

--- script.js
+++ script.js
@@ -11,28 +11,3 @@ tabs.forEach(tab => {
   })
 })
 
-/* Select all tab buttons and all content sections */
-tabs = document.querySelectorAll('[data-tab-target]');
-tabContents = document.querySelectorAll('[data-tab-content]');
-
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget);
-
-    /* 1. Reset all content sections (hide them) */
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active');
-    });
-
-    /* 2. Reset all tab titles (turn them black) */
-    tabs.forEach(t => {
-      t.classList.remove('active');
-    });
-
-    /* 3. Highlight the clicked tab (turn it pink) */
-    tab.classList.add('active');
-
-    /* 4. Show the correct content */
-    target.classList.add('active');
-  });
-});
